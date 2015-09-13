731266ba66ee4f9f25d60dafdcf090c6eacba3ae
Michał Libura <liburam@gmail.com>
2015-09-13 23:42:09 +0200

    Solution

---
+++ plot1.R
@@ -0,0 +1,7 @@
+data = read.table("household_power_consumption.txt", sep=";", header = TRUE, dec = ".", na.strings = "?")
+data = transform(data, DateTime = paste(data$Date, data$Time))
+data$DateTime = strptime(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
+feb = subset(data, DateTime>="2007-02-01" & DateTime<"2007-02-03")
+png(filename = "plot1.png")  
+hist(feb$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,7 @@
+data = read.table("household_power_consumption.txt", sep=";", header = TRUE, dec = ".", na.strings = "?")
+data = transform(data, DateTime = paste(data$Date, data$Time))
+data$DateTime = strptime(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
+feb = subset(data, DateTime>="2007-02-01" & DateTime<"2007-02-03")
+png(filename = "plot2.png")  
+plot(feb$DateTime, feb$Global_active_power, type="l", ylab = "Global active power (kilowatts)", xlab="")
+dev.off()
